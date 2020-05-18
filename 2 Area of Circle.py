a8348eae17390a6fa389e6d01456891e731586a4
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-05-18 21:20:15 +0530

    Add files via upload

---
+++ 2 Area of Circle.py
@@ -0,0 +1,11 @@
+"""
+PROBLEM 2
+Write a  program to calculate the area of a circle?
+"""
+import math
+Radius = float(input("Enter Radius : "))
+area = 2 * math.pi * Radius
+print("Area of the circle is :", area)
+
+
+
