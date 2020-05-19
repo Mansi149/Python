8909cc5aa8c72219c326ee6459c8ecfa8f122fb9
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-05-19 14:21:32 +0530

    Add files via upload

---
+++ 6 Age Calculator.py
@@ -0,0 +1,11 @@
+"""
+PROBLEM 6
+Write a program to print the output of the following problem statement :-
+Lets assume your present age is 21 today
+What would be your age after 5 years from today 
+How old were you 10 years back?
+"""
+
+Age = int(input("Enter your age : "))
+print("Your age after 5 years :", Age + 5)
+print("Your age 10 years back :", Age - 10)
