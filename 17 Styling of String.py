64706791f00e89c9d8ea67102ec7abbb00f3f7f0
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-05-23 00:23:04 +0530

    Add files via upload

---
+++ 17 Styling of String.py
@@ -0,0 +1,9 @@
+"""
+PROBLEM 17
+Write a program for styling (case change) of a given string?
+"""
+
+str = input("Enter a string : ")
+print("String in Uppercase :", str.upper())
+print("String in Lowercase :", str.lower())
+print("String in Titlecase :", str.title())
