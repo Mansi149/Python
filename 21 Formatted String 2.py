0bfa98448c1efae04edde1b22d28fa13775f9035
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-05-24 16:24:22 +0530

    Add files via upload

---
+++ 21 Formatted String 2.py
@@ -0,0 +1,9 @@
+"""
+PROBLEM 21
+Write a program to print the output of the following poblem statement :-
+Input : Welcome to Pink City Jaipur
+Output : W*e*l*c*o*m*e* *t*o* *P*i*n*k* *C*i*t*y* *J*a*i*p*u*r?
+"""
+     
+strx = input("Enter string : ")
+print("Desired string :", "*".join(strx))
