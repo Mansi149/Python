ed759040d306b0cc90b17e5332389b888165b2a4
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-05-26 23:28:29 +0530

    Add files via upload

---
+++ 27 Reverse Function.py
@@ -0,0 +1,8 @@
+"""
+PROBLEM 27
+Write a Python function to check whether a string is PANGRAM or not
+Define a function reverse() that computes the reversal of a string.
+Without using Python's inbuilt function Take input from User?
+"""
+
+input("enter string:")[::-1]
