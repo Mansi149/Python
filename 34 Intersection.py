d93e011440bd60477348afb966fc34337d2c72b9
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-05-31 00:00:39 +0530

    Add files via upload

---
+++ 34 Intersection.py
@@ -0,0 +1,13 @@
+"""
+PROBLEM 34
+Write a program to print the output of the following poblem statement :-
+With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155]
+Make a list whose elements are intersection of the above given lists ?
+"""
+
+s1 = {1,3,6,78,35,55}
+s2 = {12,24,35,24,88,120,155}
+print("Intersection of the given lists is :", list(s1.intersection(s2)))
+
+
+        
