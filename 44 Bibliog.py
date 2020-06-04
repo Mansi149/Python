8f0a983dfec2f45bc840c37c86f36d875ad4206b
Mansi149 <64794109+Mansi149@users.noreply.github.com>
2020-06-05 00:29:24 +0530

    Add files via upload

---
+++ 44 Bibliog.py
@@ -0,0 +1,108 @@
+"""
+PROBLEM 44
+Write a program to print the output of the following poblem statement :-   
+    0. Create a Book class which stores the following information about the book
+       authorlast, authorfirst, title, place, publisher and year
+       Also create a instance method write_bib_entry which 
+       returns a formatted bibliographic reference for the book
+       
+    1. Create a two instances of Book in the name of beauty and pynut
+        The Evidential Power of Beauty
+        Python in a Nutshell
+        
+    2. How would you print out the author attribute of the pynut instance
+    
+    3. If you type print beauty.write_bib_entry() at the interpreter, 
+       what will happen?
+    
+    4. How would you change the publication year for the beauty book to"2010"?
+    
+    5. Create another instance of the Book class as makeup, book of your choice
+       Execute the write_bib_entry method for that
+       
+    6. Add a method make_authoryear to the class definition that will create
+       an attribute authoryear and will set that attribute to a string that
+       has the last name of the author and then the year in parenthesis.
+       The method should not have a return statement.
+       
+    7. Create an Article class that manages information about articles. 
+       It will be very similar to the class definition for Book, except publisher
+       and place information will be unneeded and article title, volume number,
+       and pages will be needed. Make sure this class also has the methods
+       write_bib_entry and make_authoryear ?
+"""
+
+class Book(object):
+    
+    def __init__(self, authorlast, authorfirst, title, place, publisher, year):
+        self.authorlast = authorlast
+        self.authorfirst = authorfirst
+        self.title = title
+        self.place = place
+        self.publisher = publisher
+        self.year = year
+  
+    def write_bib_entry(self):
+        return self.authorlast \
+               + ', ' + self.authorfirst \
+               + ', ' + self.title + ', ' + self.place \
+               + ':  ' + self.publisher + ', ' \
+               + self.year + '.'
+
+beauty = Book( "Dubay", "Thomas", "The Evidential Power of Beauty", "San Francisco", "Ignatius Press", "1999", )
+pynut  = Book( "Martelli", "Alex", "Python in a Nutshell", "Sebastopol, CA", "O'Reilly Media, Inc.", "2003" )
+
+print(Book.write_bib_entry(pynut))
+
+print(beauty.write_bib_entry())
+
+beauty.year="2010"
+print(beauty.write_bib_entry())
+
+makeup = Book("Doe" , "John", "Good Book", "Chicago","Me Press", "2012")
+print(makeup.write_bib_entry())
+
+class Book(object):
+    def __init__(self, authorlast, authorfirst, title, place, publisher, year):
+        self.authorlast = authorlast
+        self.authorfirst = authorfirst
+        self.title = title
+        self.place = place
+        self.publisher = publisher
+        self.year = year
+
+    def write_bib_entry(self):
+        return self.authorlast \
+               + ', ' + self.authorfirst \
+               + ', ' + self.title + ', ' + self.place \
+               + ':  ' + self.publisher + ', ' \
+               + self.year + '.'
+
+    def make_authoryear(self):
+        self.authoryear = self.authorlast + '('+ self.year +' )'
+
+makeup = Book("Doe" , "John", "Good Book", "Chicago","Me Press", "2012")
+
+print(makeup.make_authoryear()) 
+
+print(makeup.authoryear)
+
+class Article(object):
+    def __init__(self, authorlast, authorfirst, title, volume, pages, year):
+        self.authorlast = authorlast
+        self.authorfirst = authorfirst
+        self.title = title
+        self.volume = volume
+        self.pages = pages
+        self.year = year
+        
+    def make_authoryear(self):
+        self.authoyear= self.authorlast + '('+ self.year +' )'
+        
+    def write_bib_entry(self):
+        return self.authorlast + ', ' + self.authorfirst \
+                               + ', ' + self.title + ', ' + self.volume \
+                               + ':  ' + self.pages + ', ' \
+                               + self.year + '.'
+
+
